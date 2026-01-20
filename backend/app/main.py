c0ffaa33ebbfde389e5c025c1c9967626248fce3
Buwanekafernando <fernandodeshan0@gmail.com>
2026-01-20 11:07:29 +0530

    regiter the analysis router

--- backend/app/main.py
+++ backend/app/main.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI
 from app.routers import chat
 from app.routers import auth
 from app.routers import face_emotion
+from app.routers import analytics
 
 app = FastAPI(
     title="AI Mental Health Chat Companion",
@@ -12,6 +13,7 @@ app = FastAPI(
 app.include_router(chat.router, prefix="/api/chat")
 app.include_router(auth.router, prefix="/api/auth", tags=["Auth"])
 app.include_router(face_emotion.router)
+app.include_router(analytics.router)
 
 @app.get("/")
 def root():
